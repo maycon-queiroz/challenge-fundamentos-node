90923c5d2cfe206bb66fd6b373eb0d9e6cb56cec
Maycon Queiroz <fmaycon@gmail.com>
2020-11-07 13:30:41 -0300

    finalized challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,20 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionTDO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
+interface GetAllTransaction {
+  transactions: Array<Transaction>;
+
+  balance: object | null;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +27,45 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): GetAllTransaction {
+    return {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(({ type }) => type === 'income')
+      .reduce((total, { value }) => total + value, 0);
+
+    const outcome = this.transactions
+      .filter(({ type }) => type === 'outcome')
+      .reduce((total, { value }) => total + value, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionTDO): Transaction {
+    const { total } = this.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error(
+        "You can't make a withdrawal because at the moment we don't have balance positive",
+      );
+    } else {
+      const transaction = new Transaction({ title, value, type });
+
+      this.transactions.push(transaction);
+
+      return transaction;
+    }
   }
 }
 
